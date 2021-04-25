1317567610a26bc65b8ebd71a630647fde8e2d78
GustavoAmimaelH <77366254+GustavoAmimaelH@users.noreply.github.com>
2021-04-24 20:13:31 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("Hellow words, Gustavo")
+
+ScrollReveal().reveal('.showcase');
+
+ScrollReveal().reveal('.news-cards', { delay:500});
+
+ScrollReveal().reveal('.cards-banner-one', { delay:500});
+
+ScrollReveal().reveal('.cards-banner-two', { delay:500});
